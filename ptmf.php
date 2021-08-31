8003d97d045371c657871c3ef78171e9dcb24f6e
Mahadi001 <sadat.himel@gmail.com>
2021-08-31 16:45:38 +0600

    update

--- ptmf.php
+++ ptmf.php
@@ -15,4 +15,19 @@ Domain Path: /languages
 function ptmf_load_texdomain(){
     load_plugin_textdomain('post-tax-metafield',false,dirname(__FILE__)."/languages");
 }
-add_action('plugin_loaded','ptmf_load_texdomain') ;
+add_action('plugin_loaded','ptmf_load_texdomain');
+
+
+function ptmf_add_metabox(){
+    add_meta_box(
+        'ptmf_select_posts_mb',
+        __('Select Posts','our-metabox'),
+        'ptmf_display_metabox',
+        array('page')
+    );
+}
+add_action('admin_menu','ptmf_add_metabox');
+
+function ptmf_display_metabox(){
+    
+}
