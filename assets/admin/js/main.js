f57eb481299b9a88d2411fd4ec7abdf0704e624f
Mahadi001 <sadat.himel@gmail.com>
2021-08-31 17:52:50 +0600

    update

---
+++ assets/admin/js/main.js
@@ -0,0 +1,91 @@
+var frame, gframe;
+;(function($){
+    $(document).ready(function() {
+
+      var imageUrl = $("#omb_image_url").val();
+      if(imageUrl){
+        $("#image_container").html(`<img src="${imageUrl}"/>`);
+      }
+
+      var imagesUrl = $("#omb_images_url").val();
+      imagesUrl = imagesUrl ? imagesUrl.split(";") : [];
+
+      for(i in imagesUrl){
+        var _imagesUrl = imagesUrl[i];
+        $("#images_container").append(`<img src="${_imagesUrl}"/>`);
+      }
+      
+
+        $(".omb_dp" ).datepicker({
+          showOtherMonths: true,
+          selectOtherMonths: true
+        });
+
+        $("#upload_image").on("click",function(){
+
+          if ( frame ) {
+            frame.open();
+            return false;
+          }
+          
+          frame = wp.media({
+            title: "Select Image",
+            button:{
+              text:"Insert Image"
+            },
+            multiple:false
+          });
+
+          frame.on('select',function(){
+           var attachment = frame.state().get('selection').first().toJSON();
+            $("#omb_image_id").val(attachment.id);
+            $("#omb_image_url").val(attachment.sizes.thumbnail.url);
+            // console.log(attachment);
+            $("#image_container").html(`<img src="${attachment.sizes.thumbnail.url}"/>`);
+          });
+
+          frame.open();
+          return false;
+        });
+
+// this section for gallery
+        $("#upload_images").on("click",function(){
+
+          if ( gframe ) {
+            gframe.open();
+            return false;
+          }
+          
+          gframe = wp.media({
+            title: "Select Images",
+            button:{
+              text:"Insert Images"
+            },
+            multiple:true
+          });
+
+          gframe.on('select',function(){
+           var attachments = gframe.state().get('selection').toJSON();
+           var image_ids = [];
+           var image_urls = [];
+           $("#images_container").html('');
+            for(i in attachments){
+              var attachment = attachments[i];
+              // console.log(attachment);
+              image_ids.push(attachment.id);
+              image_urls.push(attachment.sizes.thumbnail.url);
+              $("#images_container").append(`<img src="${attachment.sizes.thumbnail.url}"/>`);
+            }
+            console.log(image_ids,image_urls);
+             $("#omb_images_id").val(image_ids.join(";"));
+             $("#omb_images_url").val(image_urls.join(";"));
+             
+          });
+
+          gframe.open();
+          return false;
+        });
+      } );
+})(jQuery);
+
+
--- ptmf.php
+++ ptmf.php
@@ -12,10 +12,18 @@ Domain Path: /languages
 */
 
 
+
+
 function ptmf_load_texdomain(){
     load_plugin_textdomain('post-tax-metafield',false,dirname(__FILE__)."/languages");
 }
 add_action('plugin_loaded','ptmf_load_texdomain');
+
+function ptmf_admin_assets()
+{
+    wp_enqueue_style( 'omb-admin-style', plugin_dir_url(__FILE__)."assets/admin/css/style.css", null, time());    
+}
+add_action( 'admin_enqueue_scripts','ptmf_admin_assets');
 
 
 function ptmf_add_metabox(){
@@ -30,4 +38,64 @@ add_action('admin_menu','ptmf_add_metabox');
 
 function ptmf_display_metabox(){
     
+     wp_nonce_field( 'ptmf_posts', 'ptmf_posts_nonce');
+     $label = __("Select Posts","post-tax-metafield");
+     $metabox_html = <<<EOD
+        <div class="fields">
+            <div class="field_c">
+                <div class="label_c" >
+                    <label>{$label}</label>
+                </div>
+                <div class="input_c">
+                    <select name="ptmf_posts" id="ptmf_posts>
+                        <option value="0">{$label}</option>
+                    </select>
+                </div>
+            </div>
+            <div class="float_c"></div>
+        </div>  
+     EOD;
+
+     echo $metabox_html;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
